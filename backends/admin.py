5e1ed61f86e1fe724ea01e832a596f43a00017bc
Dieison <dieisoncomix@gmail.com>
2021-02-11 06:39:50 -0300

    first commit

--- backends/admin.py
+++ backends/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Institutionals, Links
+from .models import Institutionals, Links, Services
 
 # Register your models here.
 
 admin.site.register(Links)
 admin.site.register(Institutionals)
+admin.site.register(Services)
